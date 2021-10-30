71209af6a741c91e6d550725ee68f237152bff6e
TekNiBaLL <jexiao@bsu.edu>
2021-10-29 21:51:22 -0400

    update stepdefs

--- features/stepdefinitions/stepdefs.js
+++ features/stepdefinitions/stepdefs.js
@@ -1,18 +1,24 @@
 const assert = require("assert");
 const { Given, When, Then } = require("@cucumber/cucumber");
 
+
 function leader(userType) {
+  if(userType === "not"){
+    return "not";
+  }else{
+    return "";
+  }
 }
 
-Given("I am not a leader", function () {
-  this.userType = "Follower";
+Given("I am {word} a leader", function (wordd) {
+  this.userType = wordd;
 });
 
 When("I create a room", function () {
   this.actualAnswer = leader(this.userType);
 });
 
-Then('ensure room is not created', function () {
-    return "created";
+Then("ensure room is {word} created", function (expectedAnswer) {
+    assert.strictEqual(this.actualAnswer, expectedAnswer);
   });
 
